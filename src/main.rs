136eab4bea2c80efe83e4e86409e060a9f8be9d7
Arpit Yadav <arpit.y@ripenapps.in>
2022-01-27 19:10:22 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+
 fn main() {
     // println!("Hello, world!");
     let mut arguments = std::env::args().skip(1);
@@ -8,4 +11,30 @@ fn main() {
 
     let contents = format!("{}\t{}\n", key, value);
     std::fs::write("kb.db", contents).unwrap();
+
+    let database = Database::new();
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // let contents = match std::fs::read_to_string("kb.db") {
+        //     Ok(contents) => contents,
+        //     Err(error) => {
+        //         return Err(error);
+        //     }
+        // };
+        
+        let contents = std::fs::read_to_string("kb.db")?;
+
+        for line in contents.lines() {
+            let pair = line.split_once('\t').expect("Corrupted file");
+        }
+        Database {
+            map: HashMap::new();
+        }
+    } 
 }
